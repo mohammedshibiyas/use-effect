211b525f1c888b9c8430880fe136a0b3d2251114
shibiyas <shibiyas@gmail.com>
2023-11-16 11:20:27 +0530

    added

---
+++ useffect/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Test from './components/test/Test';
+
+
+function App() {
+  const [parent, setParent] = useState(0)
+
+  console.log("parent",parent);
+
+  return (
+    <>
+    <button onClick={()=>{setParent(parent+1)}}>parent {parent}</button>
+    <Test/>
+    </>
+  )
+}
+
+export default App
